15c528508aa8b6836fc1f12720a7711c46909159
Rishabh <mailrishabh1403@gmail.com>
2017-09-08 23:07:36 +0530

    change unique key

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export function fetchPosts(){
-    const request = axios.get('http://reduxblog.herokuapp.com/api/posts?key=123');
+    const request = axios.get('http://reduxblog.herokuapp.com/api/posts?key=jain');
     return {
         type : 'FETCH_POSTS',
         payload: request
